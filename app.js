d344442315d2976db10fff7a7860098228b1af27
Yogesh Jayachandran <108916534+Yogeshpj@users.noreply.github.com>
2023-03-17 11:44:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function Test()
     let myarray=["a","e","i","o"];
     let vowelArray=myarray;
     myarray.push("u");
-    console.log(myarray);
+    console.log(myarray)
     const Hi =()=>{alert("Check the console output!")}
     return<div><button onClick={Hi}>Referance Data Types and call the function</button></div>
 }
